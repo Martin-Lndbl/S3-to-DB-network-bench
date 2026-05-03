590de66af3e8a51f6ebd594b29d09a890c7f564c
Martin Lindbuechl <martin.lindbuechl@tum.de>
2026-05-03 09:03:27 +0000

    remove obsolete file

--- duckdb-minio.nix
+++
@@ -1,9 +0,0 @@
-{ writeShellScriptBin, duckdb }:
-
-writeShellScriptBin "duckdb-minio" ''
-
-  CMD="taskset -c 0 ${duckdb}/bin/duckdb -init ${./init.sql} $@"
-
-  ${duckdb}/bin/duckdb -c "INSTALL httpfs;"
-  sudo ip netns exec ns_client su -c "$CMD" $USER
-''
